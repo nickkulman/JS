b3d5b7f88a64d6c21c146b79d9c553d9345e79cb
Nickolay <nickkulman@gmail.com>
2020-03-01 22:58:54 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let num = 50;
+
+if (num < 49) {
+    console.log("Неверно!")
+} else if (num > 100) {
+    console.log("Много!")
+} else {
+    console.log("Верно!")
+};
+
+(num == 50) ? console.log("Верно!") : console.log("Неверно!");
+
+switch (num) {
+    case num < 49:
+        console.log("Неверно!");
+        break;
+    case num > 100:
+        console.log("Много!");
+        break;
+    case 50:
+        console.log("Верно!");
+        break;
+    default:
+        console.log("Что-то пошло не так!");
+        break;
+}
