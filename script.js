8bad032b9f322495d7c865f77fb178b0f0ac8004
Nickolay <nickkulman@gmail.com>
2020-03-01 23:59:54 +0300

    first homework

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let num = 50;
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-if (num < 49) {
-    console.log("Неверно!")
-} else if (num > 100) {
-    console.log("Много!")
-} else {
-    console.log("Верно!")
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
-(num == 50) ? console.log("Верно!") : console.log("Неверно!");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-switch (num) {
-    case num < 49:
-        console.log("Неверно!");
-        break;
-    case num > 100:
-        console.log("Много!");
-        break;
-    case 50:
-        console.log("Верно!");
-        break;
-    default:
-        console.log("Что-то пошло не так!");
-        break;
-}
+
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+    
+alert(appData.budget / 30);
